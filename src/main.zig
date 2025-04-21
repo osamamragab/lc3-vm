fab8faa32a4500ad9fe03b4143365fd6e45d5cd9
Osama Ragab <theosamaragab@gmail.com>
2025-04-21 19:16:03 +0200

    fixes

--- src/main.zig
+++ src/main.zig
@@ -71,11 +71,11 @@ fn memWrite(addr: u16, val: u16) void {
     memory[addr] = val;
 }
 
-fn memRead(addr: u16) u16 {
+fn memRead(addr: u16) !u16 {
     if (addr == @intFromEnum(MemoryMappedRegister.KBSR)) {
         if (checkInputKey()) {
             memory[@intFromEnum(MemoryMappedRegister.KBSR)] = 1 << 15;
-            memory[@intFromEnum(MemoryMappedRegister.KBDR)] = @as(u16, @intCast(stdin.readByte() catch 0));
+            memory[@intFromEnum(MemoryMappedRegister.KBDR)] = try stdin.readInt(u16, .big);
         } else {
             memory[@intFromEnum(MemoryMappedRegister.KBSR)] = 0;
         }
@@ -90,7 +90,7 @@ fn readImage(filename: []const u8) !void {
     var buffered_reader = io.bufferedReader(file_reader);
     var reader = buffered_reader.reader();
 
-    const org = try reader.readInt(u16, .little);
+    const org = try reader.readInt(u16, .big);
     if (org >= memory.len) {
         return error.EOF;
     }
@@ -114,11 +114,10 @@ fn swap16(n: u16) u16 {
 }
 
 fn signExtend(n: u16, bc: u4) u16 {
-    var x = n;
-    if ((x >> (bc - 1)) & 1 != 0) {
-        x |= @as(u16, 0xffff) << bc;
+    if ((n >> (bc - 1)) & 1 != 0) {
+        return n | (@as(u16, 0xffff) << bc);
     }
-    return x;
+    return n;
 }
 
 fn updateFlags(r: u16) void {
@@ -141,10 +140,7 @@ fn checkInputKey() bool {
     var readfds = FdSet.initEmpty();
     readfds.setValue(linux.STDIN_FILENO, true);
 
-    var timeout: linux.timeval = .{
-        .sec = 0,
-        .usec = 0,
-    };
+    var timeout: linux.timeval = .{ .sec = 0, .usec = 0 };
 
     return linux.syscall6(
         .pselect6,
@@ -176,133 +172,131 @@ pub fn main() !void {
         readImage(mem.span(arg)) catch @panic("failed to load image");
     }
 
-    var running = true;
-    while (running) {
-        const instr = memRead(registers[@intFromEnum(Register.PC)]);
+    loop: while (true) {
+        const instr = try memRead(registers[@intFromEnum(Register.PC)]);
         registers[@intFromEnum(Register.PC)] += 1;
         const op = instr >> 12;
         switch (@as(Opcode, @enumFromInt(op))) {
-            Opcode.BR => {
+            .BR => {
                 const pc_offset = signExtend(instr & 0x1ff, 9);
                 const cond_flag = (instr >> 9) & 0x7;
                 if (cond_flag & registers[@intFromEnum(Register.COND)] != 0) {
-                    registers[@intFromEnum(Register.PC)], _ = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset);
+                    registers[@intFromEnum(Register.PC)] = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset)[0];
                 }
             },
-            Opcode.JMP => {
+            .JMP => {
                 const r1 = (instr >> 6) & 0x7;
                 registers[@intFromEnum(Register.PC)] = registers[r1];
             },
-            Opcode.JSR => {
+            .JSR => {
                 registers[@intFromEnum(Register.R7)] = registers[@intFromEnum(Register.PC)];
                 const long_flag = (instr >> 11) & 0x1;
                 if (long_flag != 0) {
                     const pc_offset = signExtend(instr & 0x7ff, 11);
-                    const val, _ = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset);
-                    registers[@intFromEnum(Register.PC)] = val;
+                    registers[@intFromEnum(Register.PC)] = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset)[0];
                 } else {
                     const r1 = (instr >> 6) & 0x7;
                     registers[@intFromEnum(Register.PC)] = registers[r1];
                 }
             },
-            Opcode.LD => {
+            .LD => {
                 const r0 = (instr >> 9) & 0x7;
                 const pc_offset = signExtend(instr & 0x1ff, 9);
-                const addr, _ = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset);
-                registers[r0] = memRead(addr);
+                const addr = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset)[0];
+                registers[r0] = try memRead(addr);
                 updateFlags(r0);
             },
-            Opcode.LDI => {
+            .LDI => {
                 const r0 = (instr >> 9) & 0x7;
                 const pc_offset = signExtend(instr & 0x1ff, 9);
-                const addr, _ = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset);
-                registers[r0] = memRead(memRead(addr));
+                const addr = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset)[0];
+                registers[r0] = try memRead(try memRead(addr));
                 updateFlags(r0);
             },
-            Opcode.LDR => {
+            .LDR => {
                 const r0 = (instr >> 9) & 0x7;
                 const r1 = (instr >> 6) & 0x7;
                 const offset = signExtend(instr & 0x3f, 6);
-                const addr, _ = @addWithOverflow(registers[r1], offset);
-                registers[r0] = memRead(addr);
+                const addr = @addWithOverflow(registers[r1], offset)[0];
+                registers[r0] = try memRead(addr);
                 updateFlags(r0);
             },
-            Opcode.LEA => {
+            .LEA => {
                 const r0 = (instr >> 9) & 0x7;
                 const pc_offset = signExtend(instr & 0x1ff, 9);
-                registers[r0], _ = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset);
+                registers[r0] = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset)[0];
                 updateFlags(r0);
             },
-            Opcode.ST => {
+            .ST => {
                 const r0 = (instr >> 9) & 0x7;
                 const pc_offset = signExtend(instr & 0x1ff, 9);
-                const addr, _ = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset);
+                const addr = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset)[0];
                 memWrite(addr, registers[r0]);
             },
-            Opcode.STI => {
+            .STI => {
                 const r0 = (instr >> 9) & 0x7;
                 const pc_offset = signExtend(instr & 0x1ff, 9);
-                const addr, _ = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset);
-                memWrite(memRead(addr), registers[r0]);
+                const addr = @addWithOverflow(registers[@intFromEnum(Register.PC)], pc_offset)[0];
+                memWrite(try memRead(addr), registers[r0]);
             },
-            Opcode.STR => {
+            .STR => {
                 const r0 = (instr >> 9) & 0x7;
                 const r1 = (instr >> 6) & 0x7;
                 const offset = signExtend(instr & 0x3f, 6);
-                const addr, _ = @addWithOverflow(registers[r1], offset);
+                const addr = @addWithOverflow(registers[r1], offset)[0];
                 memWrite(addr, registers[r0]);
             },
-            Opcode.NOT => {
+            .NOT => {
                 const r0 = (instr >> 9) & 0x7;
                 const r1 = (instr >> 6) & 0x7;
                 registers[r0] = ~registers[r1];
                 updateFlags(r0);
             },
-            Opcode.AND => {
+            .AND => {
                 const r0 = (instr >> 9) & 0x7;
                 const r1 = (instr >> 6) & 0x7;
                 const val = if ((instr >> 5) & 0x1 != 0) signExtend(instr & 0x1f, 5) else registers[instr & 0x7];
                 registers[r0] = registers[r1] & val;
                 updateFlags(r0);
             },
-            Opcode.ADD => {
+            .ADD => {
                 const r0 = (instr >> 9) & 0x7;
                 const r1 = (instr >> 6) & 0x7;
                 const val = if ((instr >> 5) & 0x1 != 0) signExtend(instr & 0x1f, 5) else registers[instr & 0x7];
-                registers[r0], _ = @addWithOverflow(registers[r1], val);
+                registers[r0] = @addWithOverflow(registers[r1], val)[0];
                 updateFlags(r0);
             },
-            Opcode.TRAP => {
+            .TRAP => {
                 registers[@intFromEnum(Register.R7)] = registers[@intFromEnum(Register.PC)];
                 switch (@as(Trap, @enumFromInt(instr & 0xFF))) {
-                    Trap.GETC => {
+                    .GETC => {
                         registers[@intFromEnum(Register.R0)] = @as(u16, try stdin.readByte());
                         updateFlags(@intFromEnum(Register.R0));
                     },
-                    Trap.IN => {
-                        try stdout.print("Input character: ", .{});
+                    .IN => {
+                        try stdout.writeAll("Input character: ");
                         const c = try stdin.readByte();
                         try stdout.writeByte(c);
                         try stdout_writer.flush();
                         registers[@intFromEnum(Register.R0)] = @as(u16, c);
-                        updateFlags(registers[@intFromEnum(Register.R0)]);
+                        updateFlags(@intFromEnum(Register.R0));
                     },
-                    Trap.OUT => {
-                        try stdout.writeByte(@as(u8, @intCast(registers[@intFromEnum(Register.R0)])));
+                    .OUT => {
+                        try stdout.writeByte(@truncate(registers[@intFromEnum(Register.R0)]));
                         try stdout_writer.flush();
                     },
-                    Trap.PUTS => {
-                        var i = registers[@intFromEnum(Register.R0)];
-                        var c = memory[i];
-                        while (c != 0) {
-                            try stdout.writeByte(@truncate(c >> 8));
-                            try stdout.writeByte(@truncate(c & 0xff));
-                            i += 1;
-                            c = memory[i];
+                    .PUTS => {
+                        var addr = registers[@intFromEnum(Register.R0)];
+                        var char = try memRead(addr);
+                        while (char != 0) {
+                            try stdout.writeByte(@truncate(char >> 8));
+                            try stdout.writeByte(@truncate(char & 0xff));
+                            addr = @addWithOverflow(addr, 1)[0];
+                            char = try memRead(addr);
                         }
                         try stdout_writer.flush();
                     },
-                    Trap.PUTSP => {
+                    .PUTSP => {
                         var i = registers[@intFromEnum(Register.R0)];
                         var c = memory[i];
                         while (c != 0) {
@@ -316,14 +310,14 @@ pub fn main() !void {
                         }
                         try stdout_writer.flush();
                     },
-                    Trap.HALT => {
-                        try stdout.print("HALT\n", .{});
+                    .HALT => {
+                        try stdout.writeAll("HALT\n");
                         try stdout_writer.flush();
-                        running = false;
+                        break :loop;
                     },
                 }
             },
-            Opcode.RES, Opcode.RTI => @panic("unimplemented"),
+            .RES, .RTI => @panic("unimplemented"),
         }
     }
 }
